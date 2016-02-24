83b50ca8eba37780c401dc09531dc715e116c037
Andre Almeida <aalmeida@thoughtworks.com>
2016-02-24 15:53:17 -0300

    create shouldReturnStringWhenSendNumberOfAsterisks test

--- 01_PrintAsterisks/Test/com/thoughtworks/controll/DrawAsterisksTest.java
+++ 01_PrintAsterisks/Test/com/thoughtworks/controll/DrawAsterisksTest.java
@@ -4,15 +4,22 @@ package com.thoughtworks.controll;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class DrawAsterisksTest {
+  private DrawAsterisks drawAsterisks;
 
   @Before
   public void setUp() throws Exception {
-
+    this.drawAsterisks = new DrawAsterisks();
 
   }
 
-  //@Test
+  @Test
+  public void shouldReturnStringWhenSendNumberOfAsterisks(){
+    assertThat(drawAsterisks.drawAsterisks(3), is("***"));
+  }
 
 
 
--- 01_PrintAsterisks/src/com/thoughtworks/controll/DrawAsterisks.java
+++ 01_PrintAsterisks/src/com/thoughtworks/controll/DrawAsterisks.java
@@ -1,16 +1,13 @@
 package com.thoughtworks.controll;
 
-
-import com.thoughtworks.print.Printer;
-
 public class DrawAsterisks {
 
 
     public String drawAsterisks(int value){
         String asterisksStrings = "";
 
-        for (int i = 0; i <= value; i++){
-            asterisksStrings += asterisksStrings;
+        for (int i = 0; i < value; i++){
+            asterisksStrings += "*";
         }
 
         return asterisksStrings;
