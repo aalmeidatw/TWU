38495c40e655a2321b9b83bb7caddd575e286765
Andre Almeida <aalmeida@thoughtworks.com>
2016-02-24 16:23:07 -0300

    create printer test

---
+++ 02_DrawHorizontalLine/Test/com/thoughtworks/printer/PrinterTest.java
@@ -0,0 +1,30 @@
+package com.thoughtworks.printer;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.Assert.assertEquals;
+
+public class PrinterTest {
+    private Printer printer;
+    private ByteArrayOutputStream outputStream;
+
+    @Before
+    public void setUp() throws Exception {
+        this.printer = new Printer();
+        this.outputStream = new ByteArrayOutputStream();
+    }
+
+    @Test
+    public void shouldPrinterOneAsterisksInConsole(){
+
+        System.setOut(new PrintStream(outputStream));
+        String expected = "*";
+
+        printer.printerOnConsole("*");
+        assertEquals(outputStream.toString(), expected);
+
+    }
+}
