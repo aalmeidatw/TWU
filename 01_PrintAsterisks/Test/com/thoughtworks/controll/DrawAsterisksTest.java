ea2c6f275a2ae137d922826d8f9060d813e57740
Andre Almeida <aalmeida@thoughtworks.com>
2016-02-24 15:47:04 -0300

    refactoring drawAsterisks
    refactoring drawAsterisks method in drawAsterisks class

---
+++ 01_PrintAsterisks/Test/com/thoughtworks/controll/DrawAsterisksTest.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.controll;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DrawAsterisksTest {
+
+  @Before
+  public void setUp() throws Exception {
+
+
+  }
+
+  //@Test
+
+
+
+}
--- 01_PrintAsterisks/src/com/thoughtworks/controll/DrawAsterisks.java
+++ 01_PrintAsterisks/src/com/thoughtworks/controll/DrawAsterisks.java
@@ -4,14 +4,15 @@ package com.thoughtworks.controll;
 import com.thoughtworks.print.Printer;
 
 public class DrawAsterisks {
-    private Printer printer;
 
-    public void drawAsterisks(String value){
-        this.printer = new Printer();
 
-        if (value.equals("*")) {
-            printer.printerOnConsole("*");
+    public String drawAsterisks(int value){
+        String asterisksStrings = "";
 
+        for (int i = 0; i <= value; i++){
+            asterisksStrings += asterisksStrings;
         }
+
+        return asterisksStrings;
     }
 }
