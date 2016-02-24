1aad409e99ce8b5e8b421dca5d51518bad345ee2
Andre Almeida <aalmeida@thoughtworks.com>
2016-02-24 16:08:44 -0300

    create main class
    create main class to test application

--- 01_PrintAsterisks/src/com/thoughtworks/application/main.java
+++ 01_PrintAsterisks/src/com/thoughtworks/application/main.java
@@ -1,7 +1,15 @@
 package com.thoughtworks.application;
 
 
+import com.thoughtworks.controll.DrawAsterisks;
+import com.thoughtworks.print.Printer;
+
 public class main {
+
+
+    public static void main(String[] args) {
+        new Printer().printerOnConsole(new DrawAsterisks().drawAsterisks(2));
+    }
 
 
 }
